5da57a32482ed0a6078e6d25a3cc51c885c646b3
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-09-11 10:08:03 +0800

    feat: add edit and detail function to todos.js (the router)

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../../models')
-const Todo = db.todo
+const Todo = db.Todo
 
 router.get('/new', (req, res) => {
   return res.render('new')
